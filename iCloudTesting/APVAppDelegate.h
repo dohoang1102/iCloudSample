df9acce9ce87305428e5ed483d48aa19e3b08469
Michał Tuszyński <srgtuszy@gmail.com>
2011-12-05 17:48:02 +0100

    Initial Commit

---
+++ iCloudTesting/APVAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  APVAppDelegate.h
+//  iCloudTesting
+//
+//  Created by Michal Tuszynski on 12/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class APVViewController;
+
+@interface APVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) APVViewController *viewController;
+
+@end
---
+++ iCloudTesting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iCloudTesting
+//
+//  Created by Michal Tuszynski on 12/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APVAppDelegate class]));
+    }
+}
