ac494af1a020d1887fb1c3b979c4bd68436fbc56
Michał Tuszyński <srgtuszy@gmail.com>
2011-12-06 13:15:50 +0100

    Added readme and license information

--- iCloudTesting/APVViewController.m
+++ iCloudTesting/APVViewController.m
@@ -2,9 +2,19 @@
 //  APVViewController.m
 //  iCloudTesting
 //
-//  Created by Michal Tuszynski on 12/5/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+// Copyright 2011 by Michal Tuszynski
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import "APVViewController.h"
 #import "FileController.h"
@@ -68,12 +78,8 @@
     
     [self.icloudFilesArray removeAllObjects];
     
-    NSMetadataQuery *newQuery = [notification object];
-    
-    [newQuery disableUpdates];
-    [newQuery stopQuery];
-    
-    for (NSMetadataItem *item in [newQuery results]) {
+    //Iterate through all results, create documents from urls and reload the table view
+    for (NSMetadataItem *item in [self.query results]) {
         
         NSURL *fileUrl = [item valueForAttribute:NSMetadataItemURLKey];
         
@@ -117,8 +123,10 @@
 
 -(void)fetchDocumentsFromCloud {
     
+    //This predicate just fetches all files which have a file extension
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"NOT %K.pathExtension = ''", NSMetadataItemFSNameKey];
     
+    //Perform the search inside Documents iCloud scope
     self.query = [[[NSMetadataQuery alloc] init] autorelease];
     [self.query setSearchScopes:[NSArray arrayWithObjects:NSMetadataQueryUbiquitousDocumentsScope, nil]];
     [self.query setPredicate:predicate];
@@ -131,32 +139,44 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //This method should be called as soon as possible after launching the app. It returns url pointing to the iCloud-monitored 
+    //directory and enables our app to make changes there. If this method returns nil, then either you're 
+    //running on simulator or the device doesn't have iCloud enabled
     
     self.icloudUrl = [[NSFileManager defaultManager] URLForUbiquityContainerIdentifier:kStorageIdentifier];
     
-    self.filesArray = [[[NSMutableArray alloc] init] autorelease];
-    self.icloudFilesArray = [[[NSMutableArray alloc] init] autorelease];
+    if (icloudUrl) {
+        
+        self.filesArray = [[[NSMutableArray alloc] init] autorelease];
+        self.icloudFilesArray = [[[NSMutableArray alloc] init] autorelease];
     
-    [filesArray addObjectsFromArray:[FileController getFilesFromDocuments]];
+        [filesArray addObjectsFromArray:[FileController getFilesFromDocuments]];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self 
-                                             selector:@selector(metadataSearchCompleted:)
-                                                 name:NSMetadataQueryDidFinishGatheringNotification 
-                                               object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self 
+                                                 selector:@selector(metadataSearchCompleted:)
+                                                     name:NSMetadataQueryDidFinishGatheringNotification 
+                                                   object:nil];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self 
-                                             selector:@selector(metadataSearchCompleted:)
-                                                 name:NSMetadataQueryDidUpdateNotification 
-                                               object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self 
+                                                 selector:@selector(metadataSearchCompleted:)
+                                                     name:NSMetadataQueryDidUpdateNotification 
+                                                   object:nil];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self 
-                                             selector:@selector(documentStateChanged:) 
-                                                 name:UIDocumentStateChangedNotification 
-                                               object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self 
+                                                 selector:@selector(documentStateChanged:) 
+                                                     name:UIDocumentStateChangedNotification 
+                                                   object:nil];
     
-    [self fetchDocumentsFromCloud];
+        [self fetchDocumentsFromCloud];
+        
+    }
     
+    else {
+        
+        [APVUtilities spawnAlertWithMessage:@"Either you're running on simulator or don't have iCloud enabled. Check your settings and restart the app"];
+        
+    }
     
 }
 
@@ -294,29 +314,37 @@
     
     if (indexPath.section == 0) {
         
+        //All iCloud operations should be done on seperate threads, or else it might hog to main thread
         dispatch_queue_t dispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         
         dispatch_async(dispatchQueue, ^ {
             
+            //Find the file inside out app sandbox
             NSString *filePath = (NSString *)[self.filesArray objectAtIndex:indexPath.row];
             filePath = [[FileController getDocumentsDirectory] stringByAppendingPathComponent:filePath];
             
+            //Make the url out of it
             NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
             
             NSString *fileName = [NSString stringWithFormat:@"%@", [fileUrl lastPathComponent]];
+            
+            //This url points to the directory monitored by iCloud and our file is going to be copied there
             NSURL *storageUrl = [[self.icloudUrl URLByAppendingPathComponent:@"Documents"] URLByAppendingPathComponent:fileName];
             
             
             NSError *error;
             
+            //Attempt to move the file from Documents directory to the iCloud directory
             if([[NSFileManager defaultManager] setUbiquitous:YES 
                                                 itemAtURL:fileUrl 
                                            destinationURL:storageUrl 
                                                     error:&error]) {
             
             
+                //Initiate a new document with the new file path
                 Timestamp *timestamp = [[Timestamp alloc] initWithFileURL:storageUrl];
             
+                //TableView logic
                 [self.icloudFilesArray addObject:timestamp];
                 [self.filesArray removeObjectAtIndex:indexPath.row];
             
@@ -353,13 +381,7 @@
                 
                 dispatch_async(dispatch_get_main_queue(), ^ {
                    
-                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"" 
-                                                                    message:@"Something went wrong. The file failed to move to iCloud." 
-                                                                   delegate:nil 
-                                                          cancelButtonTitle:@"OK" 
-                                                          otherButtonTitles:nil];
-                    [alert show];
-                    [alert release];
+                    [APVUtilities spawnAlertWithMessage:@"Something went wrong. The file failed to move to iCloud."];
                     
                 });
                 
@@ -376,19 +398,23 @@
         dispatch_queue_t dispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         
         dispatch_async(dispatchQueue, ^ {
-            
            
+            //Grab the document from array and find it's location inside iCloud storage
             Timestamp *timestamp = (Timestamp *)[self.icloudFilesArray objectAtIndex:indexPath.row];
             
+            //The path inside app's Document directory where the file will be moved back
             NSString *targetPath = [[FileController getDocumentsDirectory] stringByAppendingPathComponent:[timestamp localizedName]];
             
             NSError *error;
             
+            //Attempt to move the file back to Document directory
             if ([[NSFileManager defaultManager] setUbiquitous:NO 
                                                     itemAtURL:[timestamp fileURL] 
                                                destinationURL:[NSURL fileURLWithPath:targetPath] 
                                                         error:&error]) {
                 
+                
+                //TableView logic
                 [self.icloudFilesArray removeObjectAtIndex:indexPath.row];
                 [self.filesArray addObject:[targetPath lastPathComponent]];
                 
@@ -420,13 +446,7 @@
                 
                 dispatch_async(dispatch_get_main_queue(), ^ {
                     
-                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"" 
-                                                                    message:@"Failed to remove document from iCloud" 
-                                                                   delegate:nil 
-                                                          cancelButtonTitle:@"OK" 
-                                                          otherButtonTitles:nil];
-                    [alert show];
-                    [alert release];
+                    [APVUtilities spawnAlertWithMessage:@"Failed to remove document from iCloud"];
                     
                 });
                 
